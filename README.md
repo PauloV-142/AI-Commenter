aa030201c287da155979d9b485900a2b2d026c9b
PauloV-142 <paulo.almeida142@etec.sp.gov.br>
2026-01-30 20:03:33 -0300

    0.2.1 Adding comments dinnamically [ONG]

--- README.md
+++ README.md
@@ -3,12 +3,34 @@ Worst Comments is a static website (localhost) that will let the user write anyt
 ---
 
 ### DONE LIST:
-- Add comments dinamically [DONE!]
+- ~Add comments dinamically~ [DONE!]
 2. Listen to textarea activity and set a threshold to add a new comment.
+    - Add a screen warning in place of the AI call. (for now)
+    - Variable timeout min-max - After this, check if the onchange trigger let call AI
+    - Define when to trigger, related to the frequency of onchange event.
+
+    :FLOW:
+    1. The user writes something.
+    2. Timeout starts: Randomly selects a waiting time between e.g. 30s and 2min
+    3. Actively listen for the frequency of change: (The user may set this threshold) - e.g. 10 to 60 changes.
+    5. Event for when the *waiting time* is over **and** there's an acceptable *frequency of changes*.
+        - Call the AI, add the comment, show a warning about it. (User PurpleOwl commented.)
+        - Reset the *frequency of change* variable.
+        Else:
+        - Don't call the AI, show a warning explaining why not.
+        - Keep the *frequency of change* variable for the next flow. (As an optional parameter for a function).
+    6. Return to the :FLOW: start.
+
 3. Align the comment section to 2x2, vertical good-looking flex.
-## Finish the first version: 7 days starting from 30/1/2026
+
+## Finish the first version: 7 days starting from 29/1/2026
 
 ### Next version:
-- Give the user text to an AI with prompt engineering
+- Give the user's text to an AI with prompt engineering
 - Create a random possibility to nested comments.
-- Save data in cache after reloading. (Try a hidden input box, just for fun)
+- Save data in cache after reloading. (Try a hidden input box, just for fun).
+- Add a notifier instead of warnings (a title && description || username && glimpse of text)
+- Make the frequency of changes fixed until the flow restarts.
+- Add comment nesting.
+- Add a *remaining time* monitor for the next AI comment
+- For saving the data in cache, let the user save different texts as different posts, one with it's own set of comments.
--- script.js
+++ script.js
@@ -1,26 +1,82 @@
 const comment_section = document.getElementById("comment_section");
 const user_text = document.getElementById("user_input");
 let commentCounter = 1;
+let addedComments = [];
 
-function insertComment(data) {
-    function newComment(data) {
-        return `<div class="comment" id="${commentCounter}">
+/*Comment adding mechanincs...*/
+function Comment(username, text) {
+    function renderComment(data) {
+        return `<div class="comment" id="${data.id}">
         <span class="username">${data.username}</span>
-        <span id="comment_id">#${commentCounter}</span>
+        <span id="comment_id">#${data.id}</span>
         <hr class="comment-sectioner">
         <p class="text">${data.text}</p>
         </div>`;
     }
-    comment_section.innerHTML += newComment(data);
-    commentCounter += 1;
-}
-
-function UserComment() {
-    this.username = window.prompt(`#${commentCounter} Apelido:`)
-    this.text = window.prompt(`Comentário:`)
+    comment_section.innerHTML += renderComment(this);
+    
+    this.username = username;
+    this.text = text;
+    this.id = ++commentCounter;
+    
+    addedComments[this.id] = this;
+    // Notify
+    console.log(addedComments)
     return this
+}
+function UserComment() {
+    username = window.prompt(`#${commentCounter} Apelido:`)
+    text = window.prompt(`Comentário:`)
+    return new Comment(username, text)
 }
 
 user_text.onclick = function() {
-    insertComment(new UserComment());
+    new UserComment()
 }
+
+/*Adding comments dinamically*/
+
+function chooseRandomTimeout(min, max) {
+    return Math.trunc(min + (Math.random() * max))
+}
+
+let flowData = {
+    timeoutId: null,
+    writingFrequency: 0,
+    writingFrequencyThreshold: 10,
+    number: 0,
+    get isEnoughText () {
+        return flowData.writingFrequency >= flowData.writingFrequencyThreshold;
+    }
+    
+};
+function flow() {
+    flowData.timeoutId = setTimeout(() => {
+        handleFlowRestarting(flowData.isEnoughText);
+        flowData.number += 1;
+        console.log("Starting new Flow:", flowData.number);
+    }, chooseRandomTimeout(30_000, 120_000));
+}
+
+function handleFlowRestarting(isEnoughText) {
+    if (isEnoughText) {
+        window.alert("Adding new comment");
+        // Add new comment
+    } else {
+        flow();
+    }
+
+}
+
+// Let flow be a function, that calls the timeout at the end
+
+// 1. The user writes something.
+user_text.onchange = function handleTextareaUpdates() {
+    if (flowData.ongoing) {
+        flowData.writingFrequency += 1;
+    } else {
+        flowData.start()
+    }
+};
+// Temporary, com 
+var exampleComments = [];
