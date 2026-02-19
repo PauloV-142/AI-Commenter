b066eb65049f332872428c64deebd5057c56802a
PauloV-142 <paulo.almeida142@etec.sp.gov.br>
2026-02-19 16:10:37 -0300

    2. Writing threshold [DONE]  D - grid, readability on JS, demo comments in Js

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Worst Comments is a static website (localhost) that will let the user write anyt
 ---
 
 ### DONE LIST:
-- ~Add comments dinamically~ [DONE!]
-2. Listen to textarea activity and set a threshold to add a new comment.
+- [DONE!] ~Add comments dinamically~ 
+- [DONE!] ~Listen to textarea activity and set a threshold to add a new comment.~ 
     - Add a screen warning in place of the AI call. (for now)
     - Variable timeout min-max - After this, check if the onchange trigger let call AI
     - Define when to trigger, related to the frequency of onchange event.
@@ -21,19 +21,29 @@ Worst Comments is a static website (localhost) that will let the user write anyt
         - Keep the *frequency of change* variable for the next flow. (As an optional parameter for a function).
     6. Return to the :FLOW: start.
 
-3. Align the comment section to 2x2, vertical good-looking flex.
+3. Align the comment section to 2xN, vertical good-looking flex.
+    - Add the newest comments to the start of the list
 
 ## Finish the first version: 7 days starting from 29/1/2026
 
 ### Next version:
 - Give the user's text to an AI with prompt engineering
-- Create a random possibility to nested comments. (using methods to existing comments.)
-- Save data in cache after reloading. (Try a hidden input box, just for fun).
-- Add a notifier instead of warnings (a title && description || username && glimpse of text)
-- Make the frequency of changes fixed until the flow restarts.
-- Add comment nesting.
-- Add a *remaining time* monitor for the next AI comment
-- For saving the data in cache, let the user save different texts as different posts, one with it's own set of comments.
-- The writing **frequency** can be worth it's name, changing it from a counter to a mean, of characters written by timeout: **writing.frequency += value / timeout** -- to make it interesting.
 - Add character counter and a minimum amount for AI generation.
 - Get random usernames from API when generating comments.
+
+- Add comment nesting.
+- Create a random possibility to nested comments. (using methods to existing comments.)
+- Add an optional *parent* parameter to the comment function, it shall add a comment *to* an already existing comment, refered by ID. Talking about IA, I shall make a prompt engineering for the AI to write a comment *as if* it was answering the existing comment's text AND using the post as reference. (change the generatedComments list to a Object for this.) --Happly, the prompt engineering looks the hardest part for me. :]
+
+- Save data in cache after reloading. (Try a hidden input box, just for fun).
+- For saving the data in cache, let the user save different texts as different posts, one with it's own set of comments.
+
+- Add a notifier instead of warnings (a title && description || username && glimpse of text)
+- Add a *remaining time* monitor for the next AI comment
+
+- Make the frequency of changes fixed until the flow restarts.
+- When the loop reaches it's end, and there isn't enough text, DON'T start another loop until the user start writing again.
+- The writing **frequency** can be worth it's name, changing it from a counter to a mean, of characters written by timeout: **writing.frequency += value / timeout** -- to make it interesting.
+- Instead of using a problematic *frequency* formula to verify if adding a comment is nedded, subtract 40 [threshold] at every generator IF the threshold has been reached, without modifying the *frequency* variable in other ways.
+
+- Add a **START/STOP** Button.
--- script.js
+++ script.js
@@ -2,28 +2,29 @@ const comment_section = document.getElementById("comment_section");
 const user_text = document.getElementById("user_input");
 const writing_frequency_counter = document.getElementById("writing_frequency_counter");
 
-let comments = [];
+let generatedComments = [];
 
 /*Comment adding mechanincs...*/
 function Comment(username, text) {
     if (! username || ! text) return;
     function renderComment(data) {
-        let id = comments.length
-        return `<div class="comment" id="${id}">
+        let id = generatedComments.length
+        const commentHTML = `<div class="comment" id="${id}">
         <span class="username">${data.username}</span>
         <span id="comment_id">#${id}</span>
         <hr class="comment-sectioner">
         <p class="text">${data.text}</p>
         </div>`;
+        comment_section.innerHTML += commentHTML;
     }
     
     this.username = username;
     this.text = text;
+    renderComment(this)
+    generatedComments.push(data)
     
-    comment_section.innerHTML += renderComment(this);
-    comments[comments.length] = this;
     // Notify: "New comment..."
-    console.log(comments)
+    console.log(generatedComments)
     return this
 }
 
@@ -43,28 +44,31 @@ function chooseRandomTimeout(min, max) {
 
 let writing = {
     freq: 0,
-    threshold: 40, // The minimal value for generating a next comment.
-    isEnoughText: false,
+    minFrequency: 40, // The minimal frequency value for generating a next comment.
+    minFreqReached: false,
+    get minFrequencyReached () {
+        if (this.frequency === this.minFrequency) {
+            this.minFreqReached = true;
+            console.log(`Minimum frequency ${this.minFrequency} reached.`);
+        }
+        return this.minFreqReached;
+    },
+    
     get frequency () {return this.freq},
     set frequency (value) {
         // Update frequency.
         this.freq = value;
         writing_frequency_counter.textContent = this.frequency;
         
-        // Monitor if threshold has been reached
-        if (this.isEnoughText) return;
-        let thresholdReached = this.frequency >= this.threshold;
-        if (thresholdReached) {
-            console.log(`Threshold ${this.threshold} reached.`);
-            this.isEnoughText = true;
-        }
+        // Check if threshold has been reached
+        this.minFrequencyReached
     },  
 };
 
 function generateComments() {
     console.log("Starting comment generation")
-    function restart(isEnoughText) {
-        if (isEnoughText) {
+    function restart(minFrequencyReached) {
+        if (minFrequencyReached) {
             new Comment("Gut", `There's enough text: ${writing.frequency}`);
             writing.frequency = 0;
         } else {
@@ -74,8 +78,28 @@ function generateComments() {
     }
 
     setTimeout(() => {
-        restart(writing.isEnoughText);
+        restart(writing.minFrequencyReached);
     }, chooseRandomTimeout(10_000, 12_000));
 }
 
 generateComments();
+
+
+// Adding random comments [TESTING]
+function commentObject(username, text) {
+    return {
+        username: username,
+        text: text
+    }
+}
+
+const myComments = [
+    commentObject("Gut", "Muito esquisito está esse texto, não consigo entender nada."),
+    commentObject("JP", "ndasçheuwoçhdbuioaphcebnejãthtiaoemefa58946317.d478a9w7d8wawdia9pey7gr9-7280-9eqjajdiawe[ajwiehw[0yr8yuogaywge6yglgwatyfetwiorastaeifwtaefwryia bngyrowfetaraçryasfwr61r379pqt0dwagdapsbcgtctozlgsdh478a9w7d8wawdia9pey7gr9-7280-9eqjajdiawe[ajwiehw[0yr8yuogaywge6yglgwatyfetwiorastaeifwtaefwryia bngyrowfetaraçryasfwr61r379pqt478a9w7d8wawdia9pey7gr9-7280-9eqjajdiawe[ajwiehw[0yr8yuogaywge6yglgwatyfetwiorastaeifwtaefwryia bngyrowfetaraçryasfwr61r379pqt478a9w7d8wawdia9pey7gr9-7280-9eqjajdiawe[ajwiehw[0yr8yuogaywge6yglgwatyfetwiorastaeifwtaefwryia bngyrowfetaraçryasfwr61r379pqt"),
+    commentObject("Bro Code", "Viu só? O texto está enlouquecendo as pessoas. Vou dar um fora daqui!"),
+    commentObject("???????", "The randomTimeout function chooses a random timeout between the parameters (num1, num2) and starts the timer to execute the function in the *func* parameter, in this example case, the function executed is restart() which will restart the randomCommentGenerator, the timer, char counter [will reset to 0 if needed] and, at the end restart anew, keeping the functional loop going.")
+]
+
+myComments.forEach((element) => {
+    new Comment(element.username, element.text)
+})
